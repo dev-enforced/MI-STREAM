c600006288edb4e79ccd1cb469cca3b4c5218aed
Roshan J <devenforced@gmail.com>
2022-04-28 14:08:33 +0530

    chore: backend data support for categories and videos

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,29 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
+    categoryName: "Product Management",
+    imgUrl: "",
     description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+      "Product management involves driving the vision, strategy, design, and execution of an organisation's product. While one can often quickly comprehend the basic responsibilities of the role, mastering each of these dimensions is truly an art form that one is constantly honing",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
+    categoryName: "Management Entrances",
+    imgUrl: "",
     description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+      "After cracking the entrance exam, your next challenge is to perform well in the WAT-PI round. It is your one shot to make an impression on your dream B-school of why you'd make a great fit in the classroom. Students who crack this round prepare themselves for personal, technical and case study questions.",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
+    categoryName: "Software Development",
+    imgUrl: "",
     description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+      "Software Engineering why use an engineering approach to software? Quite simply, because the alternative would be to use an ad-hoc or disordered approach. An engineering approach means predictability and quantifiable results through the application of theories, methodologies, frameworks, and tools. When applied efficiently, the result is high-quality software created in a cost-effective manner.",
   },
+  {
+    _id:uuid(),
+    categoryName:"Public Sector Professions",
+    imgUrl:"",
+    description:"Business Organisations/Public services run by the government for public purpose also have a competitive hiring process involving interviews testing technical abilities of an individual and how well does the candidate perform under pressure. Considered as one of the toughest learning from the experiences of ones who cleared it make the candidate get familiar with what they are going to face in the final round."
+  }
 ];
