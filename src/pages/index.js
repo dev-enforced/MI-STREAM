a6186d2e54ddac6f4883ab216b9a6fba7fbb805f
Roshan J <devenforced@gmail.com>
2022-06-01 15:40:24 +0530

    fix:build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,3 @@ export { APITester } from "./APITester/APITester";
 export { Mockbee } from "./Mockbee/Mockbee";
 export { Homepage } from "./Homepage/Homepage";
 export { ExplorePage } from "./ExplorePage/ExplorePage";
-export { Login, Signup } from "./Authentication";
