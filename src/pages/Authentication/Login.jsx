00767f6af189092915fd1a864769da222275af6b
Roshan J <devenforced@gmail.com>
2022-06-01 15:24:05 +0530

    refactor:unnecessary things

---
+++ src/pages/Authentication/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./authentication.module.css";
+const Login=()=>{
+  
+  return (
+    <></>
+  )
+}
+export {Login}
---
+++ src/pages/Authentication/Signup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./authentication.module.css";
+const Signup = () => {
+  
+  return (
+      <></>
+  );
+};
+export { Signup };
