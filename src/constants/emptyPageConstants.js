785a0c6b45f0dee601bfee31a8c98283d753ae0b
Roshan J <devenforced@gmail.com>
2022-06-11 02:28:50 +0530

    style: added images for empty stores

--- src/constants/emptyPageConstants.js
+++ src/constants/emptyPageConstants.js
@@ -1,3 +1,4 @@
 const emptyLikeImage = "/assets/Like_Empty_Section_Image.svg";
 const emptyHistoryImage = "/assets/History_empty.svg";
-export { emptyLikeImage, emptyHistoryImage };
+const emptyWatchLaterImage = "/assets/Watch_Later_Empty.svg";
+export { emptyLikeImage, emptyHistoryImage, emptyWatchLaterImage };
